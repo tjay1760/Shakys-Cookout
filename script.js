45bfeaabba2a65d9b4106c70dbbbe614131c7ca6
John Murianki <mcjthiongo@gmail.com>
2023-02-17 22:30:21 +0300

    fix margin and padding errors

--- script.js
+++ script.js
@@ -68,6 +68,7 @@ const openMenu = document.querySelector('.hamburger-icon');
 const mobileMenu = document.querySelector('.drop-menu');
 openMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden-menu');
+  document.body.classList.toggle('no-scroll');
 });
 const partners = document.querySelector('.partners');
 const morebtn = document.querySelector('.more');
--- script2.js
+++ script2.js
@@ -2,4 +2,5 @@ const openMenu = document.querySelector('.hamburger-icon');
 const mobileMenu = document.querySelector('.drop-menu');
 openMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden-menu');
+  document.body.classList.toggle('no-scroll');
 });
